06ce0dd677ee07b04020b578d5680e3be612cdb3
fujikawahiroaki <fhir0aki3@gmail.com>
2022-08-08 13:22:06 +0900

    use explicit require in model and migration template.

---
+++ src/clear/cli/generators/new/grip.cr
@@ -0,0 +1,68 @@
+require "generate"
+
+class Clear::CLI::Generator
+  register_sub_command "new:kemal", type: NewGrip, description: "Create a new project with Grip"
+
+  class NewGrip < Admiral::Command
+    include Clear::CLI::Command
+
+    define_flag directory : String,
+      default: ".",
+      long: directory,
+      short: d,
+      description: "Set target directory"
+
+    define_argument name : String
+
+    def run_impl
+      g = Generate::Generator.new
+
+      g.target_directory = flags.directory
+      g["app_name"] = arguments.name || File.basename(g.target_directory)
+
+      g["app_name_underscore"] = g["app_name"].underscore
+      g["app_name_camelcase"] = g["app_name"].camelcase
+
+      g["git_username"] = `git config user.email`.chomp || "email@example.com"
+      g["git_email"] = `git config user.name`.chomp || "Your Name"
+
+      g.in_directory "bin" do
+        g.file "appctl", Clear::CLI::Generator.ecr_to_s("#{__DIR__}/../../../../../templates/kemal/bin/appctl.ecr", g)
+        g.file "clear_cli.cr", Clear::CLI::Generator.ecr_to_s("#{__DIR__}/../../../../../templates/kemal/bin/clear_cli.cr.ecr", g)
+        g.file "server.cr", Clear::CLI::Generator.ecr_to_s("#{__DIR__}/../../../../../templates/kemal/bin/server.cr.ecr", g)
+      end
+
+      g.in_directory "config" do
+        g.file "database.yml", Clear::CLI::Generator.ecr_to_s("#{__DIR__}/../../../../../templates/kemal/config/database.yml.ecr", g)
+      end
+
+      g.in_directory "src" do
+        g.in_directory "controllers" do
+        end
+
+        g.in_directory "db" do
+          g.file "init.cr", Clear::CLI::Generator.ecr_to_s("#{__DIR__}/../../../../../templates/kemal/src/db/init.ecr", g)
+        end
+
+        g.in_directory "models" do
+          g.file "init.cr", Clear::CLI::Generator.ecr_to_s("#{__DIR__}/../../../../../templates/kemal/src/models/application_model.ecr", g)
+        end
+
+        g.file "app.cr", Clear::CLI::Generator.ecr_to_s("#{__DIR__}/../../../../../templates/kemal/src/app.ecr", g)
+      end
+
+      g.file ".gitignore", Clear::CLI::Generator.ecr_to_s("#{__DIR__}/../../../../../templates/kemal/_gitignore.ecr", g)
+      g.file "shard.yml", Clear::CLI::Generator.ecr_to_s("#{__DIR__}/../../../../../templates/kemal/shard.yml.ecr", g)
+
+      system("chmod +x #{g.target_directory}/bin/appctl")
+      system("cd #{g.target_directory} && shards")
+
+      puts "Clear + Grip template is now generated. `cd #{g.target_directory} && clear-cli server` to play ! :-)"
+    end
+  end
+end
+
+# Clear::CLI::Generator.add("new/kemal",
+#   "Setup a minimal application with kemal and clear") do |args|
+
+# end
